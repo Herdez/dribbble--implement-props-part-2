11ca520650e964f66f7d6c4c501e43933aa389a9
Herdez <abelquantikum@gmail.com>
2018-08-08 12:35:51 -0500

    Import multiple content and Inline Styles has been added to React Components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 import Header from './components/Header';
 import Shots from './components/Shots';
+import { name, year, month} from './data/Author';
 
 import './App.css';
+
+const styles = {
+	textAlign: 'center',
+	margin: '35px 0',
+	fontSize: '13px',
+	color: '#AAA'
+};
 
 class App extends Component {
   render() {
@@ -11,6 +19,7 @@ class App extends Component {
       <div>
         <Header />
         <Shots />
+        <p style={ styles }> Written by { name }, { year } </p>
       </div>
     );
   }
